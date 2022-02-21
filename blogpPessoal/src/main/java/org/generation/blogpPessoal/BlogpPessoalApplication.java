f23fe9654dc5dd0aaf3b419cab1a8273e0df9aec
NadyneBarbieri <nadynemayara@live.com>
2022-02-21 17:35:02 -0300

    blog pessoal

---
+++ blogpPessoal/src/main/java/org/generation/blogpPessoal/BlogpPessoalApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogpPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpPessoalApplication.class, args);
+	}
+
+}
---
+++ blogpPessoal/src/test/java/org/generation/blogpPessoal/BlogpPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogpPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
